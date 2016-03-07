3756baf44d620f2ed14edb2009482d74a70dfa21
zjhnys <327585907@qq.com>
2016-03-07 21:07:41 +0800

    功夫熊猫第一弹
    启动！

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+		$('#baiduacademy').fullpage({
+				sectionsColor:['red','#FF9933','#003366','yellow','red','#FF9933','#FF0033'],
+				scrollingSpeed:1000,
+				anchors:['page1','page2','page3','page4','page5','page6'],
+				css3:true,
+				navigation:true,
+				navigationPosition:'right',
+				navigationTooltips:['团队介绍','成员1','成员2','成员3','成员4','成员5'],
+				fixedElements:"#header",
+				slidesNavigation:true
+			});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
